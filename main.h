b80d4821c9bbb7e5b802a67d55ce20668153fe8a
Zeezi-maddie <zanelemadonsela31@gmail.com>
2023-03-27 07:55:01 -0700

    printf.c

--- main.h
+++ main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _putchar(char c);
 int _printf(const char *format, ...);
+int check_input(const char *format, va_list *);
+int print_output(const char *format, va_list *);
 
 #endif
